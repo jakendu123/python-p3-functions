b71d1458b3c5f36c8c91298a853083221d8fa2c2
Fredrick Okoth <fredrick.okoth1@student.moringaschool.com>
2025-05-07 08:28:35 +0300

    python-functions

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,25 @@
 
 def greet_programmer():
     pass
+ #pass
+    print("Hello, programmer!")
 
 def greet(name):
     pass
+#pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
     pass
+ #pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
     pass
+ #pass
+    return num1 + num2
 
 def halve(number):
     pass
+ #pass
+    return number / 2
